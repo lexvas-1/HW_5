ca3c6156ea3933aa60b0d0362616de2b6e1e0d79
lexvas-1 <lexsatellite@yandex.ru>
2024-07-16 19:18:35 +0300

    Задание 1 - гитхаб тест

--- src/test/java/GithHubTest.java
+++ src/test/java/GithHubTest.java
@@ -2,21 +2,26 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class GithHubTest {
 
     @BeforeAll
-static void beforeAll() {
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void exampleTest() {
-        open("/");
-
+    void enterprisePageTest() {
+        open("");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        $("[data-testid = SubNav-root-heading]").shouldHave(text("Enterprise"));
 
     }
 }
